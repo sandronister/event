fa80417b719e96795cecce363bb1c4053e202bcc
Julio Sandroni <sandronister@gmail.com>
2023-10-15 10:32:50 -0300

    create consume rabbit mq

---
+++ pkg/rabbimq/rabbimq.go
@@ -0,0 +1,39 @@
+package rabbimq
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+func OpenChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	return ch
+}
+
+func Consume(ch *amqp.Channel, out chan<- amqp.Delivery) error {
+	msgs, err := ch.Consume(
+		"orders",
+		"goconsumer",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	for msg := range msgs {
+		out <- msg
+	}
+
+	return nil
+}
