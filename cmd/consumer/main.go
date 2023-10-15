556fd9aa2b464d315cf98869b6dda08d508a70ed
Julio Sandroni <sandronister@gmail.com>
2023-10-15 10:43:30 -0300

    create a consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rabbitmq/amqp091-go"
+	"github.com/sandronister/event/pkg/rabbimq"
+)
+
+func main() {
+	ch := rabbimq.OpenChannel()
+	defer ch.Close()
+
+	msgs := make(chan amqp091.Delivery)
+	go rabbimq.Consume(ch, msgs)
+
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
